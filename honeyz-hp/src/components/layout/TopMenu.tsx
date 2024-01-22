06b91d51c1d72c578517c2fa5be78afa3921a5b4
OBeris <eppal212@naver.com>
2024-01-23 01:27:29 +0900

    탑 메뉴 개발중

--- honeyz-hp/src/components/layout/TopMenu.tsx
+++ honeyz-hp/src/components/layout/TopMenu.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import Image from "next/image";
 import { useRouter, usePathname } from "next/navigation";
+import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 // import localFont from "next/font/local";
 
@@ -12,20 +13,33 @@ import { useRouter, usePathname } from "next/navigation";
 const TopMenu = () => {
   const router = useRouter();
   const pathname = usePathname();
+  const { scrollY } = useScroll();
+  const x = useTransform(() =>
+    scrollY.get() > 78 ? 0.8 : (scrollY.get() / 78) * 0.8
+  );
+
   const menus = ["about", "news", "tealents", "guideline"];
 
   return (
-    <div className="absolute top-0 flex justify-between mt-1.5 w-full">
+    <div className="fixed top-1.5 flex justify-between w-full">
       {/* 홈 버튼 */}
       <button
-        className="py-2 px-3 rounded-r-lg border-double border-y-2 border-r-2 border-pink-500"
+        className="flex items-center px-3 h-[78px]"
         onClick={() => router.push("/")}
       >
+        <motion.div
+          className="absolute top-1/2 right-0 w-full h-full rounded-r-lg border-y-2 border-r-2 border-pink-500 bg-white z-[-1] translate-y-[-50%]"
+          style={{ opacity: x }}
+        />
         <Image src="/img/logo.png" alt="logo" width={64} height={64} />
       </button>
 
       {/* 메뉴 목록 */}
-      <div className="flex item-center gap-32 mr-32">
+      <div className="relative flex gap-32 px-32">
+        <motion.div
+          className="absolute top-1/2 right-0 w-full h-4/5 rounded-l-full border-y-2 border-l-2 border-pink-500 bg-white z-[-1] translate-y-[-50%]"
+          style={{ opacity: x }}
+        />
         {menus.map((item, index) => (
           <button
             key={index}
