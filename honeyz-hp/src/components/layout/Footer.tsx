72620e366ba7df9ba00c504968f1d141dbd8af04
OBeris <eppal212@naver.com>
2024-01-23 21:37:31 +0900

    푸터 개발

--- honeyz-hp/src/components/layout/Footer.tsx
+++ honeyz-hp/src/components/layout/Footer.tsx
@@ -1,4 +1,43 @@
+"use client";
+
+import Image from "next/image";
+import {
+  IconBrandX,
+  IconBrandYoutubeFilled,
+  IconSquareLetterN,
+} from "@tabler/icons-react";
+import MotionButton from "@/components/common/motionButton";
+
 const Footer = () => {
-  return <div>푸터</div>;
+  return (
+    <div className="flex flex-col items-center justify-center w-full gap-5 py-10 mt-40 bg-gradient-to-r from-gradiant1 via-gradiant0 to-gradiant1">
+      <div className="flex items-center gap-10">
+        <MotionButton
+          onClick={() => window.open("https://twitter.com/projecti_kr")}
+          children={<IconBrandX size={50} />}
+        />
+        <text>|</text>
+        <MotionButton
+          onClick={() => window.open("https://www.youtube.com/@Projecti_kr")}
+          children={<IconBrandYoutubeFilled size={50} />}
+        />
+        <text>|</text>
+        <MotionButton
+          onClick={() => window.open("https://cafe.naver.com/projectiofficial")}
+          children={<IconSquareLetterN size={50} />}
+        />
+      </div>
+      <div className="flex items-center gap-3">
+        <text className="text-xl font-bold">Operating by</text>
+        <Image
+          src="/img/projecti-black.png"
+          alt="projecti"
+          width={50}
+          height={50}
+        />
+      </div>
+    </div>
+  );
 };
+
 export default Footer;
