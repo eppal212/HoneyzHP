724c2aac5189db61a5b69f8978c11c4aa0c0f0f8
OBeris <eppal212@naver.com>
2024-01-23 22:28:33 +0900

    푸터 맨위로 버튼 추가

--- honeyz-hp/src/components/layout/Footer.tsx
+++ honeyz-hp/src/components/layout/Footer.tsx
@@ -2,15 +2,24 @@
 
 import Image from "next/image";
 import {
+  IconCircleArrowUp,
   IconBrandX,
   IconBrandYoutubeFilled,
   IconSquareLetterN,
 } from "@tabler/icons-react";
-import MotionButton from "@/components/common/motionButton";
+import MotionButton from "@/components/common/MotionButton";
 
 const Footer = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-full gap-5 py-10 mt-40 bg-gradient-to-r from-gradiant1 via-gradiant0 to-gradiant1">
+    <div className="relative flex flex-col items-center justify-center w-full gap-5 py-10 mt-40 bg-gradient-to-r from-gradient1 via-gradient0 to-gradient1">
+      {/* 맨 위로 버튼 */}
+      <MotionButton
+        className="absolute top-[-30px] right-20 rounded-full bg-bg"
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+        children={<IconCircleArrowUp className="text-gradient1" size={60} />}
+      />
+
+      {/* 링크 아이템 */}
       <div className="flex items-center gap-10">
         <MotionButton
           onClick={() => window.open("https://twitter.com/projecti_kr")}
@@ -27,6 +36,8 @@ const Footer = () => {
           children={<IconSquareLetterN size={50} />}
         />
       </div>
+
+      {/* 회사 로고 */}
       <div className="flex items-center gap-3">
         <text className="text-xl font-bold">Operating by</text>
         <Image
